0a1833fc8b7a15afa42cc9addf6393e31555837f
Noam Lessner <Noaml@paradox.com>
2018-05-26 23:29:04 +0300

    initial USS template viewer commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,11 +29,129 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.DodgerBlue;
+            this.chart1.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.FrameThin5;
+            chartArea2.AxisY.IsLabelAutoFit = false;
+            chartArea2.AxisY.LabelStyle.Angle = 45;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Alignment = System.Drawing.StringAlignment.Center;
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 1);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "UPS";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series4.Legend = "Legend1";
+            series4.Name = "DNS";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(446, 410);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 407);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(446, 45);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(166, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 32);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.BackColor = System.Drawing.Color.White;
+            this.radioButton1.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(303, 14);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(26, 23);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 452);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Newtonsoft.Json;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        double[][] Data = new double[2][];
+
+
         public Form1()
         {
             InitializeComponent();
+            chart1.ChartAreas[0].Axes[1].Minimum = 0;
+            chart1.ChartAreas[0].Axes[1].Maximum = 100;
+            comboBox1.DataSource= System.IO.Ports.SerialPort.GetPortNames();
+            serialPort1.NewLine = "\r\n";
+            Data[0] = new double[160];
+            Data[1] = new double[160];
+       }
+
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string ReciveString = serialPort1.ReadLine();// .Read(ReciveBuffer, offset, serialPort1.BytesToRead);
+                var suff = Newtonsoft.Json.Linq.JObject.Parse(ReciveString);
+                suff["UDS"].Values<double>().ToArray<double>().CopyTo(Data[1], 0);
+                suff["UPS"].Values<double>().ToArray<double>().CopyTo(Data[0], 0);
+                
+                chart1.BeginInvoke(new Action(() =>
+                {
+                    //                    if (chart1.BorderSkin.BackHatchStyle < ChartHatchStyle.ZigZag) chart1.BorderSkin.BackHatchStyle += 1;
+                    //                    else chart1.BorderSkin.BackHatchStyle = ChartHatchStyle.BackwardDiagonal;
+
+                    for (int i = 0; i < 1; i++)
+                    {
+                        if (chart1.ChartAreas[0].Axes[1].Maximum < Data[i].Max())
+                        {
+                            chart1.ChartAreas[0].Axes[1].Minimum = -1 * Data[i].Max();
+                            chart1.ChartAreas[0].Axes[1].Maximum = Data[i].Max();
+
+                        }
+                    }
+                    for (int i = 0; i < 1; i++)
+                    {
+                        if (chart1.ChartAreas[0].Axes[1].Minimum > Data[i].Min())
+                        {
+                            chart1.ChartAreas[0].Axes[1].Minimum = Data[i].Min();
+                            chart1.ChartAreas[0].Axes[1].Maximum = -1 * Data[i].Min();
+
+                        }
+                    }
+
+                    chart1.Series[0].Points.DataBindY(Data[0]);
+                    chart1.Series[1].Points.DataBindY(Data[1]);
+
+
+                   // chart1.Invalidate();
+                }));
+                radioButton1.BeginInvoke(new Action(() =>
+                {
+                    if (radioButton1.BackColor.ToKnownColor() != KnownColor.Green) radioButton1.BackColor = Color.FromKnownColor(KnownColor.Green);
+                    else radioButton1.BackColor = Color.FromKnownColor(KnownColor.Red);
+
+                }));
+                return;
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("{0} Exception caught.", error);
+                return;
+            }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.DiscardInBuffer();
+                serialPort1.Close();
+                button1.Text = "&Connect";
+                radioButton1.BeginInvoke(new Action(() => radioButton1.BackColor = Color.FromKnownColor(KnownColor.White)));
+/*
+                chart1.BeginInvoke(new Action(() =>
+                {
+                    chart1.BorderSkin.BackHatchStyle = 0;
+                    chart1.Series[0].Points.Clear();
+                    chart1.Series[1].Points.Clear();
+                }));
+*/
+            }
+            else
+            {
+                serialPort1.Open();
+                serialPort1.DiscardInBuffer();
+                button1.Text = "&Disconnect";
+
+
+                
+
+            }
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = System.IO.Ports.SerialPort.GetPortNames();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.SelectedValue.ToString();
         }
     }
 }
